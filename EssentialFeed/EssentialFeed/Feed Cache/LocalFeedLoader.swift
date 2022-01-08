d47dc73b502c0107c30a61428a17d462502f4580
Mate Csengeri <fic219@gmail.com>
2022-01-08 21:31:20 +0100

    Move Feed Cache production code into the EssentialFeed module

---
+++ EssentialFeed/EssentialFeed/Feed Cache/LocalFeedLoader.swift
@@ -0,0 +1,35 @@
+//
+// Created by Mate Csengeri on 2022. 01. 08. at Essential Developer
+// Copyright © 2022. Essential Developer. All rights reserved.
+//
+	
+
+import Foundation
+
+public final class LocalFeedLoader {
+    private let store: FeedStore
+    private let currentDate: () -> Date
+    
+    public init(store: FeedStore, currentDate: @escaping () -> Date) {
+        self.store = store
+        self.currentDate = currentDate
+    }
+    
+    public func save(_ items: [FeedItem], completion: @escaping (Error?) -> Void) {
+        store.deleteCachedFeed { [weak self] error in
+            guard let self = self else { return }
+            if let cacheDeletionError = error {
+                completion(cacheDeletionError)
+            } else {
+                self.cache(items, with: completion)
+            }
+        }
+    }
+    
+    private func cache(_ items: [FeedItem], with completion: @escaping (Error?) -> Void) {
+        store.insert(items, timestamp: currentDate()) { [weak self] error in
+            guard self != nil else { return }
+            completion(error)
+        }
+    }
+}
