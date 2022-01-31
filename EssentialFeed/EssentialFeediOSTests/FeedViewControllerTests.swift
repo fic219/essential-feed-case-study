d55c9d26eb5ce9c5cc3c671a951d7a8f34b55044
Mate Csengeri <fic219@gmail.com>
2022-01-31 21:11:14 +0100

    Before presenting FeedViewController, nothing isloaded.

---
+++ EssentialFeed/EssentialFeediOSTests/FeedViewControllerTests.swift
@@ -0,0 +1,30 @@
+//
+// Created by Mate Csengeri on 2022. 01. 31. at Essential Developer
+// Copyright © 2022. Essential Developer. All rights reserved.
+//
+	
+
+import XCTest
+
+final class FeedViewController {
+    init(loader: FeedViewControllerTests.LoaderSpy) {
+        
+    }
+}
+
+final class FeedViewControllerTests: XCTestCase {
+
+    func test_init_doesNotLoadFeed() {
+        let loader = LoaderSpy()
+        let sut = FeedViewController(loader: loader)
+        
+        XCTAssertEqual(loader.loadCallCount, 0)
+    }
+    
+    // MARK: - Helpers
+    
+    class LoaderSpy {
+        private(set) var loadCallCount: Int = 0
+    }
+
+}
