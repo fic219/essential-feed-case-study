29d95f0d5d1186c018a4735cff2632b39ebea8c1
Mate Csengeri <fic219@gmail.com>
2022-02-15 00:20:26 +0100

    Add `FeedImagePresenter` holding `FeedImage` presentation logic (including image loading state)

--- EssentialFeed/EssentialFeediOS/FeedPresentation/FeedImageViewModel.swift
+++ EssentialFeed/EssentialFeediOS/FeedPresentation/FeedImageViewModel.swift
@@ -4,59 +4,14 @@
 //
 	
 
-import Foundation
-import EssentialFeed
-
-final class FeedImageViewModel<Image> {
-    
-    typealias Observer<T> = (T) -> Void
-    
-    private let imageTransformer: (Data) -> Image?
-    
-    private let model: FeedImage
-    private let imageLoader: FeedImageDataLoader
-    private var task: FeedImageDataLoaderTask?
-    
-    init(model: FeedImage, imageLoader: FeedImageDataLoader, imageTransformer: @escaping (Data) -> Image?) {
-        self.model = model
-        self.imageLoader = imageLoader
-        self.imageTransformer = imageTransformer
-    }
+struct FeedImageViewModel<Image> {
+    let description: String?
+    let location: String?
+    let image: Image?
+    let isLoading: Bool
+    let shouldRetry: Bool
     
     var hasLocation: Bool {
-        return model.location != nil
-    }
-    
-    var location: String? {
-        return model.location
-    }
-    
-    var description: String? {
-        return model.description
-    }
-    
-    func preload() {
-        task = self.imageLoader.loadImageData(from: self.model.url) { result in }
-    }
-    
-    func cancel() {
-        task?.cancel()
-    }
-    
-    var onLoadStateChange: Observer<Bool>?
-    var onShowRetyStatChange: Observer<Bool>?
-    var onImageLoad: Observer<Image?>?
-    
-    func loadImage() {
-        onLoadStateChange?(true)
-        onShowRetyStatChange?(false)
-        self.task = imageLoader.loadImageData(from: model.url) { [weak self] result in
-            guard let self = self else { return }
-            self.onLoadStateChange?(false)
-            let data = try? result.get()
-            let image = data.map(self.imageTransformer) ?? nil
-            self.onShowRetyStatChange?(image == nil)
-            self.onImageLoad?(image)
-        }
+        return location != nil
     }
 }
