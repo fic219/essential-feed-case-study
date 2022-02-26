c1156d85709bfa79e2caceb7f180af7613f1e059
Mate Csengeri <fic219@gmail.com>
2022-02-26 23:50:06 +0100

    Create makeSut for FeedImagePresenterTests

--- EssentialFeed/EssentialFeedTests/Feed Presentation/FeedImagePresenterTests.swift
+++ EssentialFeed/EssentialFeedTests/Feed Presentation/FeedImagePresenterTests.swift
@@ -15,9 +15,18 @@ class FeedImagePresenter {
 class FeedImagePresenterTests: XCTestCase {
     
     func test_init_doesNotSentMessageToView() {
-        let view = ViewSpy()
-        let _ = FeedImagePresenter(view: view)
+        
+        let (_, view) = makeSut()
+        
         XCTAssertTrue(view.messages.isEmpty, "Excepted no view message")
+    }
+    
+    private func makeSut(file: StaticString = #file, line: UInt = #line) -> (sut: FeedImagePresenter, view: ViewSpy) {
+        let view = ViewSpy()
+        let sut = FeedImagePresenter(view: view)
+        trackForMemoryLeaks(view, file: file, line: line)
+        trackForMemoryLeaks(sut, file: file, line: line)
+        return (sut, view)
     }
     
     private class ViewSpy {
